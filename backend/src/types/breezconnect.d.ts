03d4633b6acc9455ad4ae4ff1646513d34155c4b
Harish Nagaraju <analystcode@gmail.com>
2025-10-29 12:00:50 +0530

    Add TypeScript definitions for BreezeConnect module

---
+++ backend/src/types/breezconnect.d.ts
@@ -0,0 +1,13 @@
+declare module "breezeconnect" {
+  export class BreezeConnect {
+    constructor();
+    generateSession(apiKey: string, secretKey: string): Promise<any>;
+    setToken(token: string): void;
+    getProfile(): Promise<any>;
+    getQuotes(params: any): Promise<any>;
+    getPortfolioHoldings(): Promise<any>;
+    placeOrder(params: any): Promise<any>;
+    modifyOrder(params: any): Promise<any>;
+    cancelOrder(params: any): Promise<any>;
+  }
+}
