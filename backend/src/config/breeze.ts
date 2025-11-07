af29b3ae81bd4e391881710e4d99ddd8e9e7342f
Harish Nagaraju <analystcode@gmail.com>
2025-11-07 13:39:34 +0530

    Update breeze.ts

--- backend/src/config/breeze.ts
+++ backend/src/config/breeze.ts
@@ -2,7 +2,9 @@ import { BreezeConnect } from 'breezeconnect';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const breeze = new BreezeConnect({ appKey: process.env.BREEZE_APP_KEY });
+//const breeze = new BreezeConnect({ appKey: process.env.BREEZE_APP_KEY });
+const breeze = new BreezeConnect();
+await breeze.generateSession(icici_api_key, icici_api_secret);
 
 export async function initBreezeSession() {
   try {
