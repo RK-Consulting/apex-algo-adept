51c23ebb4eb82a63dd5802737d9c582063ea8c2d
Harish Nagaraju <analystcode@gmail.com>
2025-11-17 10:12:23 +0530

    Implement request logger middleware
    Added a request logger middleware to log HTTP method, URL, and response time.

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -2,8 +2,22 @@
 import { Request, Response, NextFunction } from "express";
 import debug from "debug";
 
-// Enable with: DEBUG=apex:error
-const log = debug("apex:error");
+// Request logger namespace
+const requestLog = debug("apex:request");
+
+/**
+ * Logs HTTP method, URL, and response time
+ */
+export function requestLogger(req: Request, res: Response, next: NextFunction) {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const ms = Date.now() - start;
+    requestLog(`${req.method} ${req.originalUrl} ${res.statusCode} - ${ms}ms`);
+  });
+
+  next();
+}
 
 interface ApiError extends Error {
   statusCode?: number;
