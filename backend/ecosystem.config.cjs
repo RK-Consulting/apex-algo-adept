d25105bce82dc791102215c75a7e21471601ac36
Harish Nagaraju <analystcode@gmail.com>
2025-11-23 17:52:24 +0530

    Update ecosystem.config.cjs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -22,12 +22,14 @@ module.exports = {
       // (PM2 automatically loads them)
       env: {
         NODE_ENV: "development",
-        PORT: 8080
+        //PORT: 8080
+        PORT: 3000
       },
 
       env_production: {
         NODE_ENV: "production",
-        PORT: 8080
+        //PORT: 8080
+        PORT: 3000
       },
 
       autorestart: true,
