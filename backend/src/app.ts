1f136b650a5cd9324d57f682844ada94ea32657d
Harish Nagaraju <analystcode@gmail.com>
2025-11-25 17:41:18 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 // /backend/src/app.ts
 import dotenv from "dotenv";
-dotenv.config();
+//dotenv.config();
+dotenv.config({ path: "/var/www/apex-algo-adept/backend/.env" });
 import express from "express";
 //import cors from "cors";
 import helmet from "helmet";
