c9c0a967d7657d1100776be0ef9cea18f2c16dbd
Harish Nagaraju <analystcode@gmail.com>
2025-11-26 17:07:47 +0530

    Update load-env.cjs

--- backend/load-env.cjs
+++ backend/load-env.cjs
@@ -1,5 +1,5 @@
 // backend/load-env.cjs
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 // dotenv.config({ path: "/var/www/apex-algo-adept/backend/.env" });
 // Load .env before anything else
 require("dotenv").config({ path: "/var/www/apex-algo-adept/backend/.env" });
