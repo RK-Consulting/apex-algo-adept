c31545eeb354b849086fe94969c06875b59de0ac
Harish Nagaraju <analystcode@gmail.com>
2025-11-28 18:09:45 +0530

    Create ICICICallback.tsx

---
+++ src/pages/ICICICallback.tsx
@@ -0,0 +1,8 @@
+export default function ICICICallback() {
+  return (
+    <div style={{ padding: "2rem", fontSize: "1.2rem" }}>
+      <h2>ICICI Login Completed</h2>
+      <p>You may close this window.</p>
+    </div>
+  );
+}
