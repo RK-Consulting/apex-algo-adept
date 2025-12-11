2f0290d787aef98da3c518d7e25e269c52ca93ef
Harish Nagaraju <analystcode@gmail.com>
2025-12-11 15:20:54 +0530

    Update rateLimiter.ts

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -3,15 +3,15 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // 100 requests per IP
-  standardHeaders: true,
-  legacyHeaders: false,
+  max: 10, // 10 requests per IP
+  //standardHeaders: true,
+  //legacyHeaders: false,
   message: { error: "Too many requests — slow down!" },
 });
 
 // For sensitive routes like auth
 export const authLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 10,
-  message: { error: "Too many auth attempts — try later." },
+  windowMs: 60 * 1000, // 1 hour
+  max: 100,
+  message: { error: "Too many auth attempts — try ." },
 });
