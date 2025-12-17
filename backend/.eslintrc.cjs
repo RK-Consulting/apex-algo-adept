b4b59ee5333b4da4c09a39ec83fb4851b8dc76ad
Harish Nagaraju <analystcode@gmail.com>
2025-12-17 18:32:29 +0530

    Create .eslintrc.cjs

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true
+  },
+  plugins: ["import"],
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "always",
+        ts: "never"
+      }
+    ]
+  }
+};
