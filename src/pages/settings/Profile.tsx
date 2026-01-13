55361727d88612a4e92a4731d06c1d6c81dd4bed
Harish Nagaraju <analystcode@gmail.com>
2026-01-13 17:06:02 +0530

    Update Profile.tsx

--- src/pages/settings/Profile.tsx
+++ src/pages/settings/Profile.tsx
@@ -1,5 +1,4 @@
 // /src/pages/settings/Profile.tsx
-
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
